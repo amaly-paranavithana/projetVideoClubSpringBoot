5443e64501a18b219823ac04629e38afdfc81594
Rodrigue <sy.rodrigue@gmail.com>
2019-06-28 16:02:34 +0200

    rest coontroller

---
+++ videoClubBoot/src/main/java/org/formation/restController/FilmRestController.java
@@ -0,0 +1,12 @@
+package org.formation.restController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("rest/film")
+public class FilmRestController {
+
+	
+	
+}
