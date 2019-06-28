e7b78b135de51406df4f8390805845aefd7d05be
Raphaelfosse <raphael.fosse91@gmail.com>
2019-06-28 15:38:53 +0200

    done

---
+++ videoClubBoot/src/main/java/org/formation/metier/view/JsonViews.java
@@ -0,0 +1,17 @@
+package org.formation.metier.view;
+
+public class JsonViews {
+	
+	public class Common {
+		
+	};
+	
+	public class FilmAvecRealisateur extends Common {
+		
+	}
+	
+	public class RealisateurAvecFilm extends Common {
+		
+	}
+
+}
