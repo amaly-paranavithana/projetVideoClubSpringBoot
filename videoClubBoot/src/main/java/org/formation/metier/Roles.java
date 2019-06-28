02e1e91883c2f46f4cdfc56f8e6cf2428fe1caff
amaly-paranavithana <amaly.paranavithana@laposte.net>
2019-06-28 15:47:04 +0200

    ajout User, UserRole, et Roles

---
+++ videoClubBoot/src/main/java/org/formation/metier/Roles.java
@@ -0,0 +1,5 @@
+package org.formation.metier;
+
+public enum Roles {
+	ROLE_ADMIN, ROLE_USER;
+}
---
+++ videoClubBoot/src/main/java/org/formation/metier/User.java
@@ -0,0 +1,85 @@
+package org.formation.metier;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	// nom de colonne dans la base
+	@Id
+	private String username;
+	private String password;
+	private boolean enable;
+	@OneToMany(mappedBy = "user", fetch=FetchType.LAZY) //Lazy, ce n'est remonter que si on appel la méthode.
+	private List<UserRole> roles;
+
+	public User() {
+
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnable() {
+		return enable;
+	}
+
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+
+	public List<UserRole> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<UserRole> roles) {
+		this.roles = roles;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	// pour pouvoir comparer toutes les usernames
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+
+}
