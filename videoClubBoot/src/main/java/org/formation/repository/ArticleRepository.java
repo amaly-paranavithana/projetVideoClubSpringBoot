8c93798b0262e6154322a581290be728b66ad639
amaly-paranavithana <amaly.paranavithana@laposte.net>
2019-07-01 09:40:44 +0200

    ajout de la requête article -> réalisateurs

--- videoClubBoot/src/main/java/org/formation/repository/ArticleRepository.java
+++ videoClubBoot/src/main/java/org/formation/repository/ArticleRepository.java
@@ -1,8 +1,10 @@
 package org.formation.repository;
 
+import java.util.Date;
 import java.util.Optional;
 
 import org.formation.metier.Article;
+import org.formation.metier.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,4 +19,7 @@ public interface ArticleRepository extends JpaRepository<Article, Integer> {
 	
 	@Query("select distinct a from Article as a left join fetch a.emprunteur left join fetch a.film as f left join fetch f.realisateurs as r left join fetch r.key.realisateur left join fetch r.key.film")
 	Optional<Article> findAllWithEmprunteurAndFilmAndRealisateur(@Param("film")String film);
+	
+	@Query("select a from Article a left join fetch a.film f where f.dateSortie:dateSortie")
+	Optional<Film> FindByIdWithDate(Date dateSortie);
 }
