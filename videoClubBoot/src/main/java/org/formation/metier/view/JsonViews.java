f04a7b9e1a134a3b23d1e1355eaf53adc253ff81
amaly-paranavithana <amaly.paranavithana@laposte.net>
2019-07-01 10:13:01 +0200

    Ajout de JasonViews -> FilmAvecREalisateur & AdherentAvecArticle

---
+++ videoClubBoot/src/main/java/org/formation/metier/view/JsonViews.java
@@ -0,0 +1,15 @@
+package org.formation.metier.view;
+
+public class JsonViews {
+	
+	public static class Common {
+
+	};
+	public static class FilmAvecRealisateur extends Common{
+		
+	};
+	public static class AdherentAvecArticle extends Common{
+		
+	};
+
+}
